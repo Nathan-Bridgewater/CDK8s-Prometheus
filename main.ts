87a64ae8d7552d314aba76ca677c08126b7c93a1
Nathan Bridgewater <nathan.bridgewater@accenture.com>
2023-12-18 20:25:22 +0000

    Added config map and data volumes to deployment

--- main.ts
+++ main.ts
@@ -1,6 +1,17 @@
 import { Construct } from 'constructs'
 import { App, Chart, ChartProps } from 'cdk8s'
-import { Namespace, ClusterRole, ApiResource, NonApiResource, ClusterRoleBinding, ServiceAccount } from 'cdk8s-plus-25'
+import { join } from 'path'
+import { 
+  Namespace, 
+  ClusterRole, 
+  ApiResource, 
+  NonApiResource, 
+  ServiceAccount, 
+  Deployment, 
+  Volume,
+  ConfigMap} 
+from 'cdk8s-plus-25'
+
 
 export class MyChart extends Chart {
   constructor(scope: Construct, id: string, props: ChartProps = { }) {
@@ -8,7 +19,7 @@ export class MyChart extends Chart {
 
     const namespace = new Namespace(this, 'monitoring')
 
-    const clusterRole = new ClusterRole(this, 'prometheus-cluster-role')
+    const clusterRole = new ClusterRole(this, 'cluster-role')
 
     clusterRole.allowRead(ApiResource.NODES) 
     clusterRole.allowRead(ApiResource.SERVICES)
@@ -17,10 +28,29 @@ export class MyChart extends Chart {
     clusterRole.allowRead(ApiResource.INGRESSES)
     clusterRole.allowGet(NonApiResource.of('/metrics'))
 
-    const serviceAccount = new ServiceAccount(this, 'prometheus-service-account')
-
+    const serviceAccount = new ServiceAccount(this, 'service-account')
+    
     clusterRole.bind(serviceAccount)
+    
+    const configMap = new ConfigMap(this, 'configmap')
+    configMap.addFile(join(__dirname + 'prometheus.yml'))
 
+    const configVolume = Volume.fromConfigMap(this, 'config-volume', configMap)
+    const dataVolume = Volume.fromEmptyDir(this, 'data-volume', 'data')
+    
+    const deployment = new Deployment(this, 'deployment', {
+      containers: [ 
+        {
+          name: 'prometheus',
+          image: 'prom/prometheus',
+          args: ['--config.file=/etc/prometheus/prometheus.yml'],
+          portNumber: 9090,
+        }
+      ]
+    })
+
+    deployment.containers[0].mount('/etc/prometheus/', configVolume)
+    deployment.containers[0].mount('/data/', dataVolume)
   
   }
 }
